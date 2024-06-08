14c0ab6f3ba843f361c0a74b8e9606139a291a1b
Afonso Costa <afonso2004costa@gmail.com>
2024-06-08 22:54:18 +0100

    Add project files.

---
+++ DWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
